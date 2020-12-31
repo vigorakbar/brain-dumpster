652c3f24e4bdf6ce5a28d2746c5244334eace786
Vigor Akbar <vigorakbar@gmail.com>
2020-12-31 19:42:40 +0700

    add config page

---
+++ src/components/Configs/Configs.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import { Button, Container, makeStyles, Typography } from '@material-ui/core';
+import ArrowDown from '@material-ui/icons/ArrowDownwardRounded'
+import ArrowUp from '@material-ui/icons/ArrowUpwardRounded'
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    height: 'calc(100vh - 64px)',
+    '@media (max-width: 600px)': {
+      height: 'calc(100vh - 56px)',
+    },
+    overflow: 'hidden'
+  },
+  title: {
+    borderBottom: '2px solid lightgrey',
+    marginTop: '18px',
+    paddingBottom: '6px',
+    width: '100%',
+  },
+  importExport: {
+    margin: '32px 0'
+  },
+  imExWrapper: {
+    margin: '24px 0'
+  }
+}));
+
+const Config = () => {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <Container>
+        <Typography variant="h4" className={classes.title}>
+          Configs
+        </Typography>
+        <div className={classes.importExport}>
+          <div className={classes.imExWrapper}>
+            <Typography variant="body1"><b>Import</b> Database from JSON file</Typography>
+            <Button variant="contained" color="primary" startIcon={<ArrowDown />}>Import</Button>
+          </div>
+          <div>
+            <Typography variant="body1"><b>Export</b> Database to JSON file</Typography>
+            <Button variant="contained" color="primary" startIcon={<ArrowUp />}>Export</Button>
+          </div>
+        </div>
+      </Container>
+    </div>
+  );
+};
+
+export default Config;
