a6b6b013524eb34fcc48c14977539fafc448b9f6
Vigor Akbar <vigorakbar@gmail.com>
2020-12-31 19:42:03 +0700

    refactor navbar

--- src/App.js
+++ src/App.js
@@ -1,97 +1,38 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
-import DeleteIcon from '@material-ui/icons/Delete';
-import FolderIcon from '@material-ui/icons/Folder';
 import GarbageDump from './components/GarbageDump/GarbageDump';
-import { Switch, Route, useHistory } from 'react-router-dom';
-import { Drawer, List, ListItem, ListItemIcon, ListItemText } from '@material-ui/core';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import Archive from 'components/Archive/Archive';
+import { ARCHIVE_PAGE, BRAIN_DUMPSTER_PAGE, CONFIG_PAGE } from 'const/pages';
+import Config from 'components/Configs/Configs';
+import NavBar from 'components/NavBar/NavBar';
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles(() => ({
   root: {
     flexGrow: 1,
     position: 'relative',
     minHeight: '100vh',
   },
-  menuButton: {
-    marginRight: theme.spacing(2),
-  },
-  title: {
-    flexGrow: 1,
-  },
-  list: {
-    width: 250,
-    marginTop: 16,
-  }
 }));
 
 export default function App(props) {
   const classes = useStyles();
-  const history = useHistory();
-  const [openDrawer, setOpenDrawer] = useState(false)
 
   return (
     <div className={classes.root}>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            className={classes.menuButton}
-            onClick={() => setOpenDrawer(true)}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" className={classes.title}>
-            Brain Dumpster
-          </Typography>
-          <Button color="inherit">Login</Button>
-        </Toolbar>
-      </AppBar>
-      <Drawer
-        anchor="left"
-        open={openDrawer}
-        onClose={() => setOpenDrawer(false)}
-      >
-        <div
-          className={classes.list}
-          role="presentation"
-          onClick={() => setOpenDrawer(false)}
-          onKeyDown={() => setOpenDrawer(false)}
-        >
-          <List>
-            <ListItem
-              button
-              selected={window.location.pathname === '/'}
-              onClick={() => history.push('/')}
-            >
-              <ListItemIcon><DeleteIcon /></ListItemIcon>
-              <ListItemText primary="Brain Dump" />
-            </ListItem>
-            <ListItem
-              button
-              selected={window.location.pathname === '/archive'}
-              onClick={() => history.push('/archive')}
-            >
-              <ListItemIcon><FolderIcon /></ListItemIcon>
-              <ListItemText primary="Garbage Archive" />
-            </ListItem>
-          </List>
-        </div>
-      </Drawer>
+      <NavBar />
       <Switch>
-        <Route exact path="/">
+        <Route exact path='/' >
+          <Redirect to={BRAIN_DUMPSTER_PAGE} />
+        </Route>
+        <Route exact path={BRAIN_DUMPSTER_PAGE}>
           <GarbageDump />
         </Route>
-        <Route exact path="/archive">
+        <Route exact path={ARCHIVE_PAGE}>
           <Archive />
+        </Route>
+        <Route exact path={CONFIG_PAGE}>
+          <Config />
         </Route>
       </Switch>
     </div>
---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,101 @@
+import React, { useState } from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import DeleteIcon from '@material-ui/icons/Delete';
+import FolderIcon from '@material-ui/icons/Folder';
+import SettingsIcon from '@material-ui/icons/Settings';
+import { Drawer, List, ListItem, ListItemIcon, ListItemText, makeStyles } from '@material-ui/core';
+import { ARCHIVE_PAGE, BRAIN_DUMPSTER_PAGE, CONFIG_PAGE } from 'const/pages';
+import { useHistory } from 'react-router-dom';
+
+const useStyles = makeStyles((theme) => ({
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+  list: {
+    width: 250,
+    marginTop: 16,
+  }
+}));
+
+const NavBar = () => {
+  const classes = useStyles();
+  const history = useHistory();
+  const [openDrawer, setOpenDrawer] = useState(false)
+
+  return (
+    <>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            className={classes.menuButton}
+            onClick={() => setOpenDrawer(true)}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            Brain Dumpster
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        anchor="left"
+        open={openDrawer}
+        onClose={() => setOpenDrawer(false)}
+      >
+        <div
+          className={classes.list}
+          role="presentation"
+          onClick={() => setOpenDrawer(false)}
+          onKeyDown={() => setOpenDrawer(false)}
+        >
+          <List>
+            <ListItem
+              button
+              selected={window.location.pathname === BRAIN_DUMPSTER_PAGE}
+              onClick={() => history.push(BRAIN_DUMPSTER_PAGE)}
+            >
+              <ListItemIcon>
+                <DeleteIcon />
+              </ListItemIcon>
+              <ListItemText primary="Brain Dump" />
+            </ListItem>
+            <ListItem
+              button
+              selected={window.location.pathname === ARCHIVE_PAGE}
+              onClick={() => history.push(ARCHIVE_PAGE)}
+            >
+              <ListItemIcon>
+                <FolderIcon />
+              </ListItemIcon>
+              <ListItemText primary="Garbage Archive" />
+            </ListItem>
+            <ListItem
+              button
+              selected={window.location.pathname === CONFIG_PAGE}
+              onClick={() => history.push(CONFIG_PAGE)}
+            >
+              <ListItemIcon>
+                <SettingsIcon />
+              </ListItemIcon>
+              <ListItemText primary="Configs" />
+            </ListItem>
+          </List>
+        </div>
+      </Drawer>
+    </>
+  );
+};
+
+export default NavBar;
